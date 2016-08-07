d723bd4fa60de458f551b960cf2406f4ad9e5cef
Eddie Hu <huistheman@gmail.com>
2016-08-07 01:12:12 -0700

    Update dotfiles

--- .aliases.sh
+++ .aliases.sh
@@ -4,6 +4,13 @@ alias lplg="lpass ls | grep"
 alias lps="lpass show -c --password"
 alias lpsync="lpass sync -b"
 
+# apt-get
+alias aar="add-apt-repository"
+alias agar="apt-get autoremove"
+alias api="apt-get install"
+alias agr="apt-get remove"
+alias agu="apt-get update"
+
 # Files
 alias l="ls -o -hX --group-directories-first"
 alias la="ls -o -AhX --group-directories-first"
